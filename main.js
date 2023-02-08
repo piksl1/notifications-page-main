ebddcbd83d1804609c7ce318713dbf66f3a4aea6
Wassim Zaoui <75567675+piksl1@users.noreply.github.com>
2023-02-08 17:00:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,3 @@ readMark.addEventListener("click", function () {
   readMark.style.color = "hsl(1, 90%, 64%)";
   notificationCounter.innerHTML = 0;
 });
-
-// function reseter() {
-//   if (changeReadMarkColor) {
-//     return ();
-//   }
-// }
-// window.addEventListener("load", () => {
-//   reseter();
-// });
